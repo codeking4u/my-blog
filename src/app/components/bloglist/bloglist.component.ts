b7d6f1a466a4ae54179ac34df4a6f31fbe35b434
codeking4u <codeking4u@gmail.com>
2019-03-15 15:39:16 -0400

    limit added

--- src/app/components/bloglist/bloglist.component.ts
+++ src/app/components/bloglist/bloglist.component.ts
@@ -8,13 +8,19 @@ import { BlogsService } from '../../services/blogs.service';
   styleUrls: ['./bloglist.component.css']
 })
 export class BloglistComponent implements OnInit {
-  blogs;
+  blogs=[];
+  page:number= 1;
   constructor(private blogsService:BlogsService) { }
 
   ngOnInit() {
-    this.blogsService.getPosts().subscribe(blogs=>{
-      this.blogs=blogs;
+    this.blogsService.getPosts(this.page).subscribe(bloglists=>{
+      console.log(bloglists)
+      this.blogs = this.blogs.concat(bloglists);
+      
     });
+  }
+  loadMore(){
+    this.page = this.page+1;
   }
   
 }
--- src/app/services/blogs.service.ts
+++ src/app/services/blogs.service.ts
@@ -3,11 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class BlogsService {
-  blogsURL = 'https://5c8a6f760861d300146f73eb.mockapi.io/api/blogs?page=1&limit=10';
+  blogsURL = 'https://5c8a6f760861d300146f73eb.mockapi.io/api/blogs';
   constructor( private http:HttpClient) { }
-  getPosts():Observable<any>{
-    console.log(this.http.get(this.blogsURL))
-    return this.http.get(this.blogsURL);
+  getPosts(page):Observable<any>{
+    return this.http.get(this.blogsURL+'?page='+page+'&limit=10');
     
   }
 }
